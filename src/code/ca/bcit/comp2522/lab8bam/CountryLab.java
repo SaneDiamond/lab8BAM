76e2e5859fad865759fbe4eee704afa8c8c2c88a
SaneDiamond <andredizon3@gmail.com>
2024-10-31 12:11:46 -0700

    created path to the matches dir and week8countries.txt

--- src/code/ca/bcit/comp2522/lab8bam/CountryLab.java
+++ src/code/ca/bcit/comp2522/lab8bam/CountryLab.java
@@ -1,7 +1,33 @@
 package ca.bcit.comp2522.lab8bam;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class CountryLab {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(final String[] args) throws IOException {
+
+        final Path pathMatches;
+        final Path pathCountries;
+
+        pathMatches = Paths.get("src", "code", "ca", "bcit", "comp2522", "lab8bam", "matches");
+        pathCountries = Paths.get("week8countries.txt");
+        if(Files.notExists(pathMatches)) {
+            Files.createDirectory(pathMatches);
+        }
+
+        if(Files.notExists(pathCountries)){
+            throw new IOException("File not found.");
+        }
+
+        try {
+            final List<String>countries;
+            countries = Files.readAllLines(pathCountries);
+        } catch (final IOException e){
+            e.printStackTrace();
+        }
+
     }
 }
