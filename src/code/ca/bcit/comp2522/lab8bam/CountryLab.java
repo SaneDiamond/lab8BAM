de9b8efa17461b1d8f3712947fcb2a4375253efd
SaneDiamond <andredizon3@gmail.com>
2024-11-06 10:22:37 -0800

    did my part :D

--- src/code/ca/bcit/comp2522/lab8bam/CountryLab.java
+++ src/code/ca/bcit/comp2522/lab8bam/CountryLab.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Stream;
@@ -40,5 +41,40 @@ public class CountryLab {
                 .filter(Objects::nonNull)
                 .filter(p -> !p.isBlank() && p.isEmpty());
 
+    }
+
+    private static Stream<String> countriesShorterThan5Letters(final List<String> countries) {
+        return countries.stream()
+                .filter(Objects::nonNull)
+                .filter(p -> p.length() > 5);
+    }
+
+    private static Stream<String> countriesContainingUnited(final List<String> countries) {
+        return countries.stream()
+                .filter(Objects::nonNull)
+                .filter(p -> !p.isBlank() && p.contains("United"));
+    }
+
+    private static Stream<Character> uniqueLetters(final List<String> countries){
+        return countries.stream()
+                .filter(Objects::nonNull)
+                .filter(p -> !p.isBlank())
+                .flatMap(p -> p.chars().mapToObj(c -> (char) c))
+                .distinct();
+    }
+
+    private static Stream<String> shortestCountryName(final List<String> countries){
+        return countries.stream()
+                .filter(Objects::nonNull)
+                .filter(p -> !p.isBlank())
+                .min(Comparator.comparingInt(String::length))
+                .stream();
+    }
+
+    private static Stream<String> countryNamesToCount(final List<String> countries){
+        return countries.stream()
+                .filter(Objects::nonNull)
+                .filter(p -> !p.isBlank())
+                .map(country -> String.format("%s: %d characters", country, country.length()));
     }
 }
